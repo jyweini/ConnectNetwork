36bc3d102f74296104c3fc75225bc9a1320ab022
jyweini <jyweini@163.com>
2015-08-02 14:24:10 +0800

    HttpClient

---
+++ app/src/main/java/com/example/administrator/nettest/HttpClientActivity.java
@@ -0,0 +1,63 @@
+package com.example.administrator.nettest;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import static com.example.administrator.nettest.R.id.clientBt;
+
+
+public class HttpClientActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_http_client);
+        Button bt= (Button) this.findViewById(clientBt);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Thread td=new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        HttpClient cliet=new DefaultHttpClient();
+                        HttpGet get=new HttpGet("http://120.27.46.196:8082/bianMeile/user/getAllUser.action?mobile=13053549598");
+                        try {
+                            HttpResponse response=cliet.execute(get);
+                            HttpEntity entity=response.getEntity();
+                            InputStream is=entity.getContent();
+                            BufferedReader br=new BufferedReader(new InputStreamReader(is));
+                            String line=null;
+                            StringBuffer sb=new StringBuffer();
+                            while((line=br.readLine())!=null){
+                                sb.append(line);
+                            }
+                            System.out.println(sb.toString());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                });
+                td.start();
+            }
+        });
+    }
+
+
+
+}
--- app/src/main/java/com/example/administrator/nettest/HttpURLConnectActivity.java
+++ app/src/main/java/com/example/administrator/nettest/HttpURLConnectActivity.java
@@ -9,12 +9,15 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 
 
 public class HttpURLConnectActivity extends ActionBarActivity {
@@ -30,13 +33,22 @@ public class HttpURLConnectActivity extends ActionBarActivity {
                 Thread td=new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        String urlStr="http://120.27.46.196:8082/bianMeile/user/getAllUser.action?mobile=13053549598";
+                        String urlStr="http://120.27.46.196:8082/bianMeile/user/getAllUser.action";
                         HttpURLConnection  conn;
                         URL rul= null;
                         try {
                             rul = new URL(urlStr);
                             conn = (HttpURLConnection) rul.openConnection();
+                            conn.setDoOutput(true);
+                            conn.setDoInput(true);
+                            conn.setRequestMethod("POST");
+                            conn.setUseCaches(false);
                             conn.connect();
+                            OutputStream os=conn.getOutputStream();
+                            DataOutputStream dos=new DataOutputStream(os);
+                            dos.writeBytes("name="+ URLEncoder.encode("ÄãºÃ","UTF-8"));
+                            dos.writeBytes("mobile=13053549598");
+                            dos.close();
                             InputStream is = conn.getInputStream();
                             BufferedReader br = new BufferedReader(new InputStreamReader(is));
                             String line = null;
